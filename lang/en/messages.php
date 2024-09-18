654a91bd37a139c335a2f6698ce11e478b946395
Luan Pham <luanpm88@gmail.com>
2024-09-18 14:47:04 +0700

    sdk trans

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -3,9 +3,13 @@
 return [
     'app_name' => 'EmailEVS',
     'app_domain' => 'app.emailevs.com',
-    'app_key' => 'emailevs', // emailevs-php, emailevs-c-sharp
+    'app_sdk_name' => 'emailevs',
+    'app_sdk_php' => 'emailevs-php',
+    'app_sdk_c_sharp' => 'emailevs-c-sharp',
 
     // 'app_name' => 'AthenaEVS',
     // 'app_domain' => 'athenaevs.com',
-    // 'app_key' => 'athenaevs', // athenaevs-php, athenaevs-c-sharp
+    // 'app_sdk_name' => 'athenaevs',
+    // 'app_sdk_php' => 'athenaevs-php',
+    // 'app_sdk_c_sharp' => 'athena-c-sharp',
 ];
